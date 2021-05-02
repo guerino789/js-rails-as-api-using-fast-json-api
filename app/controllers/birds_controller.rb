bda34524495dd60d0512a4069076d2b2f7d3a7a2
guerino antonini <guerantonini94@gmail.com>
2021-05-01 20:02:30 -0400

    commit tree 98b2f9ceb1e9d56578bf35c6541356b9a80c6c28

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,12 @@
 class BirdsController < ApplicationController
+
+    def show
+        bird = Bird.find(params[:id])
+        render json: BirdSerializer.new(bird)
+      end
+    
+      def index
+        birds = Bird.all
+        render json: BirdSerializer.new(birds)
+      end
 end
